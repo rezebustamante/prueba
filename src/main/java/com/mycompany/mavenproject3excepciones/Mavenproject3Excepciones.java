e5818075bba51adf3ac2fcc33a569ce6ca7b1054
Punto Digital <Punto Digital@SFE-CERES-11>
2023-11-02 20:17:29 -0300

    version 1.0

---
+++ src/main/java/com/mycompany/mavenproject3excepciones/Mavenproject3Excepciones.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject3excepciones;
+
+/**
+ *
+ * @author Punto Digital
+ */
+public class Mavenproject3Excepciones {
+
+    public static void main(String[] args) {
+        /* int a = 10;
+        int b = 0;
+        int resultado = a/b;
+        System.out.println(resultado);*/
+        
+        /*int vector[] = new int [5];
+        try 
+        {
+          for (int i=0; i<=5; i++)
+        {
+            vector[i]=i+10;
+        }
+        }
+        catch (Exception error)
+        {
+            System.out.println("Algó pasó perro");
+        }*/
+    } 
+}
